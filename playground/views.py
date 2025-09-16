31196a29d107f6af5720a7c37e90e94fff619152
Akmal Khan <akmalnawabi007@gmail.com>
2025-09-16 10:18:58 +0330

    new changes

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
 
-def cal():
-  x = 1
-  y = 2
-  return x + y
-
 def say_hello(request):
-  cal()
-  return render(request, 'hello.html', {'name': 'Akmal', 'x': cal()})
+  return render(request, 'hello.html', {'name': 'Akmal'})
