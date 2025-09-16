8ac4ecee524cc9f3d012fc8eb3a3325c0ce3b958
Akmal Khan <akmalnawabi007@gmail.com>
2025-09-16 15:15:27 +0330

    new

--- second_project/second_app/models.py
+++ second_project/second_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class MenuItems(models.Model):
+  name = models.CharField(max_length=200)
+  price = models.IntegerField()
+  
+class Reservation(models.Model):
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  guest_count = models.IntegerField()
+  reservation_time = models.DateField(auto_now=True)
+  comments = models.CharField(max_length=200)
