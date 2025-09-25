f8edd88fc20add89e6fc270d3612171bd37b7432
Akmal Khan <akmalnawabi007@gmail.com>
2025-09-25 11:07:44 +0330

    new

--- second_project/second_app/urls.py
+++ second_project/second_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('function/', views.say_hello)
+  path('function/', views.say_hello),
+  path('reservation/', views.home)
 ]
--- second_project/second_app/views.py
+++ second_project/second_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ReservationForm
 
 def say_hello(request):
   return HttpResponse('Hello, World!')
+
+
+def home(request):
+  form = ReservationForm()
+  
+  if request.method == 'POST':
+    form = ReservationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponse('success')
+    
+  return render(request, 'index.html', {'form': form})
