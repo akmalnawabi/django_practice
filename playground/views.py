d548f888d2e683b08920c9bcea41803aa4db3cf3
Akmal Khan <akmalnawabi007@gmail.com>
2025-09-30 09:07:09 +0330

    new url admin

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from shop.models import Product
 
 def say_hello(request):
+  
+  query_set = Product.objects.all()
+  print(query_set)
   return render(request, 'hello.html', {'name': 'Akmal'})
